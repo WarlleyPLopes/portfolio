fb96f67f73885257b3628627299559f489bcd128
Warlley Lopes <warlleylopes123@gmail.com>
2025-01-09 09:58:59 +0000

    updating code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,4 @@
 export function Header() {
-  const about = () => {
-    window.scrollTo({
-      top: document.querySelector("#about").offsetTop,
-      behavior: "smooth",
-    })
-  }
-  const skills = () => {
-    window.scrollTo({
-      top: document.querySelector("#skills").offsetTop,
-      behavior: "smooth",
-    })
-  }
-
-  const contato = () => {
-    window.scrollTo({
-      top: document.querySelector("#contato").offsetTop,
-      behavior: "smooth",
-    })
-  }
   return (
     <div className="w-screen bg-gray-900">
       <div className="max-w-7xl h-24 flex items-center justify-between m-auto">
@@ -25,30 +6,10 @@ export function Header() {
           <p className="text-2xl font-bold hover:text-red-700">Warlley Lopes</p>
         </div>
         <div>
-          <div className="flex gap-4">
-            <button
-              type="button"
-              className="hover:font-medium scroll-smooth"
-              onClick={about}
-            >
-              About
-            </button>
-
-            <button
-              type="button"
-              className="hover:font-medium scroll-smooth"
-              onClick={skills}
-            >
-              Skills
-            </button>
-
-            <button
-              type="button"
-              className="hover:font-medium scroll-smooth"
-              onClick={contato}
-            >
-              Contact
-            </button>
+          <div className="flex gap-4">        
+            <a href="#about" className="hover:font-medium scroll-smooth">About</a>
+            <a href="#skills" className="hover:font-medium scroll-smooth">Skills</a>
+            <a href="#contato" className="hover:font-medium scroll-smooth">Contact</a>
           </div>
         </div>
       </div>
